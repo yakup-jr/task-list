c10fb318de50dce183de9252c848fe0f7c1775d4
Dmitry <dimaakupovz@gmail.com>
2023-01-22 17:58:36 +0400

    feat(admin app): remove welcome
    * remove welcome component
    * remove unit test for welcome
    * change strategy on onPush
    
    Close #2

--- apps/frontend/admin/src/app/app.component.ts
+++ apps/frontend/admin/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 
 @Component({
 	selector: 'task-list-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
 	title = 'frontend-admin'
