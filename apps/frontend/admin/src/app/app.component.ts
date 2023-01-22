e66ba91bc56099b53135cd9efc1ac3d2b919ee86
Dmitry <dimaakupovz@gmail.com>
2023-01-22 14:56:04 +0400

    feat(admin app): init admin app
    * add admin app
    
    #2

---
+++ apps/frontend/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'task-list-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+	title = 'frontend-admin'
+}
