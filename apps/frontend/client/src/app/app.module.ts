12af9602aa38d5fb2b671d8d520ec685bf538caa
Dmitry <71725152+yakup-jr@users.noreply.github.com>
2023-01-23 23:04:22 +0400

    Revert "feat(client app): add router"

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { RouterModule } from '@angular/router'
-import { appRoutes } from './app.routes'
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [
-		BrowserModule,
-		RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })
-	],
+	imports: [BrowserModule],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
