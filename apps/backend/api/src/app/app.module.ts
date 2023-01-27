8686d4e233059324fe87d3c82b2477a963342cf6
Dmitry <dimaakupovz@gmail.com>
2023-01-27 18:48:52 +0400

    feat(db): add PostgresQL db
    * install dependencies
    * import module typeorm to app.module
    * setup file .env
    * create db
    * write script for docker db
    
    Close #19

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { environment } from '../environment/environment'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-	imports: [],
+	imports: [
+		TypeOrmModule.forRoot({
+			...environment.connection
+		})
+	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService]
 })
 export class AppModule {}
