f6b782a75990ab23fdff633eb0caeb0599078ef4
Dmitry <71725152+yakup-jr@users.noreply.github.com>
2023-03-13 23:30:55 +0400

    feat(api-auth): add auth module
    * change auth module
       * add jwt.strategy.ts
       * add jwt const => environment.ts
       * change auth.service.ts
    * add to .env jwt options
    
    #27

--- apps/backend/api/src/app/auth/services/auth.service.ts
+++ apps/backend/api/src/app/auth/services/auth.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common'
 @Injectable({})
 export class AuthService {
 	login(signInPayload: SighAuthPayload): Promise<SighAuthResponse> {
-		return
+		return {
+			accessToken, // JWT
+			expiresIn, // JWT
+			id, // user.service.ts
+		}
 	}
 }
--- apps/backend/api/src/environment/environment.ts
+++ apps/backend/api/src/environment/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
 	production: false,
+	jwt: {
+		secret: process.env.JWT_SECRET,
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	},
 	connection: {
 		type: process.env.DB_TYPE as 'aurora-postgres',
 		host: process.env.DB_HOST,
