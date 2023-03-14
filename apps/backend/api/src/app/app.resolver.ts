e1416220b8b984eb1c081c92910789c71588daed
Dmitry <71725152+yakup-jr@users.noreply.github.com>
2023-03-15 00:02:19 +0400

    feat(api-auth): add resolver scalar & update imports [WiP]
    * change auth.module.ts
       * add imports
    * change app.module.ts
       * update imports
    * change app.resolver.ts
       * add resolverMap
    
    #12

--- apps/backend/api/src/app/app.resolver.ts
+++ apps/backend/api/src/app/app.resolver.ts
@@ -1,4 +1,31 @@
-import { Resolver } from '@nestjs/graphql'
+import { GraphQLScalarType } from 'graphql'
+import GraphQLJSON from 'graphql-type-json'
+import { Kind } from 'graphql/language'
 
-@Resolver('app')
-export class AppResolver {}
+export const resolverMap = {
+	Data: new GraphQLScalarType({
+		name: 'Date',
+		description: 'Date custom scalar type',
+		serialize(value) {
+			if (value instanceof Date) {
+				return value.getTime() // value from the client
+			}
+			throw Error('GraphQL Date Scalar serializer expected a `Date` object')
+		},
+		parseValue(value) {
+			if (typeof value === 'number') {
+				return new Date(value) // value send to the client
+			}
+			throw new Error('GraphQL Date Scalar parser expected a `number`')
+		},
+		parseLiteral(ast) {
+			if (ast.kind === Kind.INT) {
+				// ast value is always in string format
+				return new Date(parseInt(ast.value, 10))
+			}
+			// Invalid hard-coded value (not an integer)
+			return null
+		},
+	}),
+	JSON: GraphQLJSON,
+}
--- apps/backend/api/src/app/auth/auth.module.ts
+++ apps/backend/api/src/app/auth/auth.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common'
+import { UserService } from '../users/services/user.service'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { environment } from '../../environment/environment'
+import { AuthService } from './services/auth.service'
+import { PasswordService } from './services/password.service'
+import { JwtStrategy } from './strategy/jwt.strategy'
+import { AuthResolver } from './resolvers/auth.resolver'
 
 @Module({
-	imports: [],
-	providers: [],
-	exports: [],
+	imports: [
+		UserService,
+		PassportModule.register({
+			defaultStrategy: 'jwt',
+		}),
+		JwtModule.register({
+			privateKey: environment.jwt.secret,
+			signOptions: {
+				expiresIn: environment.jwt.expiresIn,
+			},
+		}),
+	],
+	providers: [AuthService, PasswordService, JwtStrategy, AuthResolver],
+	exports: [AuthService, PassportModule],
 })
 export class AuthModule {}
