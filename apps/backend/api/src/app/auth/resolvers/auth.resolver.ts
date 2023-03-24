1e05079b21ec6f709caeeb8ab632e8e79a9f65fb
Dmitry <71725152+yakup-jr@users.noreply.github.com>
2023-03-24 22:01:56 +0400

    docs(backend-api): add docs for auth module [WiP]
    * add comment
    => auth.decorators.ts
    => auth.resolver.ts
    => auth.service.ts
    => password.service.ts
    => jwt.strategy.ts
    => auth.graphql
    => auth.module.ts
    
    # 30

--- apps/backend/api/src/app/auth/resolvers/auth.resolver.ts
+++ apps/backend/api/src/app/auth/resolvers/auth.resolver.ts
@@ -8,15 +8,20 @@ import { AuthService } from '../services/auth.service'
 
 @Resolver('Auth')
 export class AuthResolver {
+	// get service
 	constructor(private readonly authService: AuthService) {}
 
+	// resolve login
 	@Query('login')
 	async login(
+		// get required params from request
 		@SignIn() signInPayload: SignAuthPayload
 	): Promise<SignAuthResponse> {
+		// try to find and return user
 		return await this.authService.login(signInPayload)
 	}
 
+	// resolve loguot
 	@Query('logout')
 	async logout(): Promise<boolean> {
 		return true
--- apps/backend/api/src/app/auth/services/auth.service.ts
+++ apps/backend/api/src/app/auth/services/auth.service.ts
@@ -11,17 +11,20 @@ import {
 
 @Injectable({})
 export class AuthService {
+	// get services for auth
 	constructor(
 		private readonly jwtService: JwtService,
 		private readonly userService: UserService,
 		private readonly passwordService: PasswordService
 	) {}
 
+	// checking validity of username and password
 	async validateUser(
 		username: string,
 		password: string
 	): Promise<Omit<UserModel, 'password'>> {
 		const user = await this.userService.findByUsername(username)
+		// password comparison
 		const isValid = user
 			? await this.passwordService.compare(password, user.password)
 			: false
@@ -34,16 +37,21 @@ export class AuthService {
 	}
 
 	async login(signInPayload: SignAuthPayload): Promise<SignAuthResponse> {
+		// get valid user
 		const user = await this.validateUser(
 			signInPayload.username,
 			signInPayload.password
 		)
+		// unauthorized user
 		if (!user) throw new UnauthorizedException()
 		const payload = { username: user.username, id: user.id }
 
 		return {
+			// send payload for create jwt token: headers.payload.secret
 			accessToken: this.jwtService.sign(payload),
+			// when expires
 			expiresIn: new Date(environment.jwt.expiresIn).getDate(),
+			// user id
 			id: user.id,
 		}
 	}
