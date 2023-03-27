3a7fbe8b23ad3ed348f4cb9ba27bb8e771aa504c
Dmitry <71725152+yakup-jr@users.noreply.github.com>
2023-03-27 10:23:27 +0400

    feat(frontend-admin * package.json): add ngrx [WiP]
    * change app.module.ts
       * add import ngrx generate with nx cli
    * package.json
       * add ngrx dependency
    
    #39

--- apps/frontend/admin/src/app/app.module.ts
+++ apps/frontend/admin/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { RouterModule } from '@angular/router'
 import { appRoutes } from './app.routes'
+import { StoreModule } from '@ngrx/store'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreRouterConnectingModule } from '@ngrx/router-store'
 
 @NgModule({
 	declarations: [AppComponent],
@@ -11,6 +14,18 @@ import { appRoutes } from './app.routes'
 		BrowserModule,
 		// register router, enabledBlocking - blocks rendering of the application until the initial navigation is complete
 		RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+		StoreModule.forRoot(
+			{},
+			{
+				metaReducers: [],
+				runtimeChecks: {
+					strictActionImmutability: true,
+					strictStateImmutability: true,
+				},
+			}
+		),
+		EffectsModule.forRoot([]),
+		StoreRouterConnectingModule.forRoot(),
 	],
 	providers: [],
 	bootstrap: [AppComponent],
