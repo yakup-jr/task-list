a803378e98c69aa06f5cdb22aea07c150ce87ef6
Dmitry <71725152+yakup-jr@users.noreply.github.com>
2023-03-27 15:04:29 +0400

    fix(frontend-client): init ssr [WiP]
    ## Careful dependency in this commit does not install
    
    * change app.module.ts
       * remove imports examples libs
       * remove bootstrap property
    * add app.browser.module.ts
       * add imports
       * setup browser devtools
       * add bootstrap
    * add app.server.module.ts
       * add imports
       * add bootstrap
    * app.component.html
       * remove tags from example libs
    * add environment.ts
       * define production property
    * add environmetn.prod.ts
    * change main.ts
       * rename => main.browser.ts
       * add settings for ssr
    * add main.server.ts
       * add exports for ssr
    * change project.json
       * change path to output
       * change path to main file
    * add server.ts
       * init for ssr
    * change tsconfig.app.json
       * change indent
       * change path to main file
    * add tsconfig.server.json
    
    #40

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -3,21 +3,13 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { AppComponent } from './app.component'
-import { FrontendExampleModule } from '@task-list/frontend-example'
-import { FrontendExample2Module } from '@task-list/frontend-example2'
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
-		BrowserModule,
-		// import module from libs
-		FrontendExampleModule,
-		// import module from libs
-		FrontendExample2Module,
+		BrowserModule.withServerTransition({ appId: 'serverApp' }),
 		// import module from material-ui
 		MatSlideToggleModule,
 	],
-	providers: [],
-	bootstrap: [AppComponent],
 })
 export class AppModule {}
