4fdcc13d0a3e3125974e45660162bd96c9365e10
Dmitry <71725152+yakup-jr@users.noreply.github.com>
2023-04-07 21:01:49 +0400

    feat(frontend-client): add router
    * add app.routes.ts
    * app.module.ts
       * init router
    
    #45

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -4,14 +4,15 @@ import { BrowserModule } from '@angular/platform-browser'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { AppComponent } from './app.component'
 import { RootStoreModule } from '@task-list/frontend/shared/data-access/root-store'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
-		// import module from material-ui
 		MatSlideToggleModule,
 		RootStoreModule,
+		RouterModule.forRoot([], { initialNavigation: 'enabledNonBlocking' }),
 	],
 	providers: [],
 	bootstrap: [AppComponent],
